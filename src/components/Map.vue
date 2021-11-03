a458fa5bb6d1275c30ffb7a3bbcb5018fc720890
Sensaur <zhigarev@bk.ru>
2021-11-03 10:48:29 +0500

    start

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,55 +1,62 @@
 <template>
-    <div class="map">
-        <h3>Карта офиса</h3>
+  <div class="map">
+    <h3>Карта офиса</h3>
 
-        <div
-            v-if="!isLoading"
-            class="map-root"
-        >
-            <!-- map -->
-        </div>
-        <div v-else>Loading...</div>
+    <div
+        v-if="!isLoading"
+        class="map-root"
+    >
+      <MapSVG/>
     </div>
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
 <script>
+import MapSVG from '@/assets/images/map.svg';
+
 export default {
-    data() {
-        return {
-            isLoading: false,
-        };
-    },
+  components: {
+    MapSVG,
+  },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
 };
+
+
 </script>
 
 <style scoped>
 .map {
-    height: 100%;
-    width: 100%;
-    padding: 24px;
-    overflow: hidden;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
+  height: 100%;
+  width: 100%;
+  padding: 24px;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 }
 
 .map-root {
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-    box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 
 ::v-deep svg {
-    height: 100%;
-    width: 100%;
+  height: 100%;
+  width: 100%;
 }
 
 ::v-deep .table {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
