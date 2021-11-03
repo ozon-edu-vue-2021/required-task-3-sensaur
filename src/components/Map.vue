ac683c395407f7be2bfef0f0ca54849cbde50ba7
Sensaur <zhigarev@bk.ru>
2021-11-03 19:00:41 +0500

    фейсом об тэйбл

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,50 +2,85 @@
   <div class="map">
     <h3>Карта офиса</h3>
 
-    <div
-        v-if="!isLoading"
-        class="map-root"
-    >
-      <MapSVG/>
+    <div v-if="!isLoading" class="map-root">
+      <MapSVG ref="svg" />
+      <Table v-show="false" ref="table" />
     </div>
     <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-import MapSVG from '@/assets/images/map.svg';
-import * as d3 from 'd3';
+import MapSVG from "@/assets/images/map.svg";
+import tables from "@/assets/data/tables.json";
+import Table from "@/assets/images/workPlace.svg";
+import legend from "@/assets/data/legend.json";
+
+import * as d3 from "d3";
 export default {
   components: {
     MapSVG,
+    Table,
   },
   data() {
     return {
       isLoading: false,
       svg: null,
       g: null,
+      tableSVG: null,
+      tables: [],
     };
   },
   mounted() {
-    //на маунтед чтобы гарантировано было дерево
-    this.svg = d3.select(this.$refs.svg)
-    this.g = this.svg.select('g')
-    if(this.g) {
-      this.drawTables()
+    this.isLoading = true;
+
+    this.svg = d3.select(this.$refs.svg);
+    this.g = this.svg.select("g");
+    this.tableSVG = d3.select(this.$refs.table)
+    if (this.g) {
+      this.drawTables();
     } else {
-      console.log('ERROR')
+      console.log("ERROR");
     }
-    console.log(this.svg, this.g)
+    this.isLoading = false;
+  },
+  created() {
+    this.tables = tables;
   },
   methods: {
     drawTables() {
-      const svgTablesGroup = this.g.append('g').classed('groupPlaces', true)
-      console.log(svgTablesGroup)
-    }
+      const svgTablesGroupPlace = this.g
+        .append("g")
+        .classed("groupPlaces", true);
+      this.tables.map((table) => {
+        const targetSeat = svgTablesGroupPlace
+          .append("g")
+          .attr("transform", `translate(${table.x}, ${table.y}) scale(0.5)`)
+          .attr("id", table._id)
+          .classed("employer-place", true);
+
+        targetSeat
+          .append("g")
+          .attr("transform", `rotate(${table.rotate || 0})`)
+          .attr("group_id", table.group_id)
+          .classed("table", true)
+          .html(this.tableSVG.html());
+
+        targetSeat
+            .append("g")
+            .attr("transform", `rotate(${table.rotate || 0})`)
+            .attr("group_id", table.group_id)
+            .classed("table", true)
+            .html(this.tableSVG.html())
+            .attr( // устанавливаем цвет подразделения
+                "fill",
+                legend.find((it) => it.group_id === table.group_id)
+                    ?.color ?? "transparent"
+            );
+      });
+    },
   },
 };
-
-
 </script>
 
 <style scoped>
