d3e5b5cb4a420085b41a52c00a675d35029684d6
Ilya Zhigarev <zhigarev@bk.ru>
2021-11-04 20:58:38 +0500

    люди пересчитываются

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,7 +4,7 @@
 
     <div v-if="!isLoading" class="map-root">
       <MapSVG ref="svg" />
-      <Table v-show="false" ref="table" />
+      <Table v-show="false" ref="table" v-bind="$props" />
     </div>
     <div v-else>Loading...</div>
   </div>
@@ -17,6 +17,7 @@ import Table from "@/assets/images/workPlace.svg";
 import legend from "@/assets/data/legend.json";
 
 import * as d3 from "d3";
+
 export default {
   components: {
     MapSVG,
@@ -36,7 +37,7 @@ export default {
 
     this.svg = d3.select(this.$refs.svg);
     this.g = this.svg.select("g");
-    this.tableSVG = d3.select(this.$refs.table)
+    this.tableSVG = d3.select(this.$refs.table);
     if (this.g) {
       this.drawTables();
     } else {
@@ -52,31 +53,29 @@ export default {
       const svgTablesGroupPlace = this.g
         .append("g")
         .classed("groupPlaces", true);
+
       this.tables.map((table) => {
         const targetSeat = svgTablesGroupPlace
           .append("g")
           .attr("transform", `translate(${table.x}, ${table.y}) scale(0.5)`)
           .attr("id", table._id)
-          .classed("employer-place", true);
+          .classed("employer-place", true)
+          .on("click", function () {
+            console.log(this.id);
+          });
 
         targetSeat
           .append("g")
           .attr("transform", `rotate(${table.rotate || 0})`)
           .attr("group_id", table.group_id)
           .classed("table", true)
-          .html(this.tableSVG.html());
-
-        targetSeat
-            .append("g")
-            .attr("transform", `rotate(${table.rotate || 0})`)
-            .attr("group_id", table.group_id)
-            .classed("table", true)
-            .html(this.tableSVG.html())
-            .attr( // устанавливаем цвет подразделения
-                "fill",
-                legend.find((it) => it.group_id === table.group_id)
-                    ?.color ?? "transparent"
-            );
+          .html(this.tableSVG.html())
+          .attr(
+            // устанавливаем цвет подразделения
+            "fill",
+            legend.find((it) => it.group_id === table.group_id)?.color ??
+              "transparent"
+          );
       });
     },
   },
